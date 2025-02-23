773c3840761aebb7299d089ca78d170c38e07777
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-02-22 17:32:54 -0700

    chore: add gh-pages as a dev dependency and initialize related files

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -12,19 +12,31 @@ import { WinnerModal } from './components/WinnerModal.jsx';
 function App() {
 
   //Este conjunto de estados y funciones se aplica a cada ellemento Square (a los 9 individualmente) y por ende cada uno puede activarlo individualmente
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board")
+    return boardFromStorage?  JSON.parse(boardFromStorage) :  Array(9).fill(null)
+  } );
+
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn")
+    return turnFromStorage ?? TURNS.X
+  });
+
 
   //null es que no hay ganador, false es empate, (X u O es winner pero so agregados por omision )
   const [winner, setWinner] = useState(null)
 
 
-  const [turn, setTurn] = useState(TURNS.X);
 
   const resetGame = () => {
     //Resetar todos los estados (3 en este caso) - no actualizar la pagina
     setBoard(Array(9).fill(null)),
     setTurn(TURNS.X),
     setWinner(null)
+    
+    window.localStorage.removeItem("board")
+    window.localStorage.removeItem("turn")
   }
 
   const updateBoard = (index) => {
@@ -38,6 +50,11 @@ function App() {
     //Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn)
+
+    //Guardar aqui la partida
+    window.localStorage.setItem("board", JSON.stringify(newBoard))
+    window.localStorage.setItem("turn", newTurn)
+
     //Revisar si hay un ganador en cada turno.
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
